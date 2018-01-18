7618fe09f01ff148fe0aa7089bb34bd70ff0fcef
Administrator <yc-liuyonglin@163.com>
2018-01-18 14:02:18 +0800

    etest

---
+++ test/src/main/java/com/test/Test.java
@@ -0,0 +1,5 @@
+package com.test;
+import com.lyl.common.util.AesPwdUtil;;
+public class Test {
+
+}
---
+++ test/src/main/java/com/test/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * file_name: package-info.java
+ * project_name: test
+ * author: lyl
+ * Date: 2016年7月3日
+ */
+package com.test;
